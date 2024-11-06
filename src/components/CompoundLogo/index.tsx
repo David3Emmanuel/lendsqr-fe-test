7b677d10276945561699c91373328ae73559d07b
David Emmanuel <david3emmanuel@gmail.com>
2024-11-06 12:45:30 +0100

    fix: link compound logo to home page

--- src/components/CompoundLogo/index.tsx
+++ src/components/CompoundLogo/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import style from './style.module.scss'
 import logo from '@/app/icon.svg'
+import Link from 'next/link'
 
 export default function CompoundLogo({ className }: { className?: string }) {
   return (
-    <div className={`${style.CompoundLogo} ${className || ''}`}>
+    <Link href='/' className={`${style.CompoundLogo} ${className || ''}`}>
       <Image src={logo} alt='Logo' height={25} />
       <h1>lendsqr</h1>
-    </div>
+    </Link>
   )
 }
