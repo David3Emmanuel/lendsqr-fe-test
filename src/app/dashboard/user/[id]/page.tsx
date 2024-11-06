7f3f5a74823e7775da16ed471ea88597d509ba97
David Emmanuel <david3emmanuel@gmail.com>
2024-11-06 23:48:41 +0100

    add user header section to user details page

--- src/app/dashboard/user/[id]/page.tsx
+++ src/app/dashboard/user/[id]/page.tsx
@@ -1,24 +1,60 @@
 import { getUser, UserData } from '@/utils'
+import Link from 'next/link'
+import style from './style.module.scss'
+import Button from '@/components/Button'
 
-export default async function UserDetailPage({
+export default async function UserDetailsPage({
   params,
 }: {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }) {
-  const { id } = params
+  const { id } = await params
   const user: UserData | null = await getUser(id)
 
   if (!user) return <div>User not found</div>
 
   return (
     <div>
-      <h1>User Detail for {user.username}</h1>
-      <p>Email: {user.email}</p>
-      <p>Phone: {user.phone}</p>
-      <p>Status: {user.status}</p>
-      <p>First Name: {user.firstName}</p>
-      <p>Date Joined: {user.dateJoined}</p>
-      <p>Organization: {user.organization}</p>
+      <Link href='/dashboard/users' className={style.backToUsers}>
+        <i className='fa-solid fa-arrow-left' />
+        Back to Users
+      </Link>
+      <div className={style.titleRow}>
+        <h1>User Details</h1>
+        <div className={style.actions}>
+          <Button className={style.blacklist}>Blacklist User</Button>
+          <Button className={style.activate}>Activate User</Button>
+        </div>
+      </div>
+      <div className={style.userHeader}>
+        <div className={style.basicInfo}>
+          <div className={style.avatar}>
+            <i className='fa-solid fa-user' />
+          </div>
+          <div>
+            <h2>{user.username}</h2>
+            <p>{user.id}</p>
+          </div>
+          <div className={style.divider} />
+          <div className={style.tier}>
+            <p>User's Tier</p>
+            {/* TODO add stars */}
+            <p>Tier {user.tier}</p>
+          </div>
+          <div className={style.divider} />
+          <div className={style.bank}>
+            <h2>
+              {user.bankDetails.balance.toLocaleString(undefined, {
+                style: 'currency',
+                currency: 'NGN',
+              })}
+            </h2>
+            <h6>
+              {user.bankDetails.accountNumber}/{user.bankDetails.bankName}
+            </h6>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
