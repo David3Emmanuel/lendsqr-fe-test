ea950530997c39977a031f7bd108a705131dff9d
David Emmanuel <david3emmanuel@gmail.com>
2024-11-06 23:32:04 +0100

    update user details interface

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,49 @@
 export interface UserData {
-    id: string
     email: string
     phone: string
+    id: string
     status: string
     username: string
     firstName: string
+    gender: string
+    lastName: string
     dateJoined: string
     organization: string
+    tier: 1 | 2 | 3
+
+    bankDetails: {
+        balance: number
+        accountNumber: string
+        bankName: string
+    }
+    personal: {
+        maritalStatus: string
+        children: number
+        residence: string
+        bvn: string
+    }
+    employmentDetails: {
+        levelOfEducation: string
+        employment: string | undefined
+        sector: string | undefined
+        duration: number | undefined
+        monthlyIncome: {
+            average: number
+            rangePercent: number
+        }
+        loanRepayment: number
+    }
+    socials: {
+        twitter: string | undefined
+        instagram: string | undefined
+        facebook: string | undefined
+    }
+    guarantors: {
+        fullName: string
+        email: string
+        relationship: string
+        phone: string
+    }[]
 }
 
 export async function getAllUsers(): Promise<UserData[]> {
@@ -18,7 +55,24 @@ export async function getAllUsers(): Promise<UserData[]> {
             },
         },
     )
-    return response.json()
+    const users: UserData[] = await response.json()
+
+    console.log(users[0].personal)
+
+    users.forEach((user) => {
+        user.socials = {
+            twitter:
+                user.socials.twitter ||
+                `@${user.firstName.toLowerCase()}_${user.lastName.toLowerCase()}`,
+            instagram:
+                user.socials.instagram ||
+                `@${user.firstName.toLowerCase()}_${user.lastName.toLowerCase()}`,
+            facebook:
+                user.socials.facebook || `${user.firstName} ${user.lastName}`,
+        }
+    })
+
+    return users
 }
 
 export async function getUser(id: string): Promise<UserData | null> {
